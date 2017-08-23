cdc005b6dcc3c1170e70b41d9072e130bce05ecd
brunagirao <30693791+brunagirao@users.noreply.github.com>
2017-08-22 21:25:38 -0300

    commit

---
+++ spring-data-livraria/src/main/java/com/example/springdatalivraria/SpringDataLivrariaApplication.java
@@ -0,0 +1,12 @@
+package com.example.springdatalivraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataLivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataLivrariaApplication.class, args);
+	}
+}
